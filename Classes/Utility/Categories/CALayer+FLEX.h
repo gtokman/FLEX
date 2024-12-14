f6bfd657af681860d781bda8290ebe788c9d3f89
Gary Tokman <yo@6ary.com>
2024-12-14 17:25:26 -0500

    Update CALayer+FLEX.h

--- Classes/Utility/Categories/CALayer+FLEX.h
+++ Classes/Utility/Categories/CALayer+FLEX.h
@@ -5,11 +5,3 @@
 //  Created by Tanner on 2/28/20.
 //  Copyright © 2020 FLEX Team. All rights reserved.
 //
-
-#import <QuartzCore/QuartzCore.h>
-
-@interface CALayer (FLEX)
-
-@property (nonatomic) BOOL flex_continuousCorners;
-
-@end
